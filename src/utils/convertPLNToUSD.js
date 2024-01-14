3a273b905719b4ff883ca3d1af3f66ea80c4ad06
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-14 23:50:15 +0100

    Initial commit

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,21 @@
+export const convertPLNToUSD = (PLN) => {
+
+    if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+        return 'Error';
+    }
+    if (typeof PLN === 'string' || isNaN(PLN)) {
+        return NaN;
+    }
+    if (PLN < 0) {
+        return '$0.00';
+    }
+
+    const PLNtoUSD = PLN / 3.5;
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
